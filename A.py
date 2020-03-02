b37dd624d91deb6d1c41529eb098f7c42c2594a0
Asmera21 <60932534+Asmera21@users.noreply.github.com>
2020-03-02 11:22:36 -0500

    Update A.py

--- A.py
+++ A.py
@@ -1,9 +1,62 @@
+wordBank = ['sahib', 'water', 'pine', 'physics', 'mouse', 'rhyme', 'history', 'committee', 'keyboard', 'calabresi', 'anir', 'flag', 'test', 'break', 'perpendicular', 'cosine']
+
+
+def initialize():
+    
+    space = "__ "
+    
+    print("Start guessing")
+    
+    print(space) * len(word)
+    
+    
+  
+def getLetter():
+    
+    letter = raw_input("What is your first guess? ")
+    global letter
+    
+    letterList = []
+    
+    global letterList
+    
+    letterList.append(letter)
+
+    
+        
 def checkLetter():
     global updatedSpaces
     if letter in word:
-        checkifWon()
+        checkIfWon()
         updatedSpaces = updatedSpaces -1
         
     else:
         lives = lives -1
-        getLetter()
+        getLetter()        
+        
+            
+def checkIfWon():
+                 
+        if len(letterList) == len(word):
+            
+            print("You win yay")
+            
+        else:
+            
+            getLetter()
+                            
+                               
+                                  
+def main():
+    
+    initialize()
+
+    getLetter()
+    
+    checkIfWon()
+    
+main()
+    
+ 
+                                                                                                      
+
